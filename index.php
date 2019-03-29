fc0716bd852e4555c3891cc898a5f0dbec4cfe18
Diya-P <37181269+Diya-P@users.noreply.github.com>
2019-03-29 17:56:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <?php 
 error_reporting(1);
   if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['contactno']) && isset($_POST['msg']))
-	{
-		$name =  $_POST['name'];
-		$email=	 $_POST['email'];
- 		$contactno = $_POST['contactno'];
- 		$msg =  $_POST['msg'];
-	 	if(!empty($name) && !empty($email) && !empty($contactno) && !empty($msg) )
-  		  {  
+  {
+    $name =  $_POST['name'];
+    $email=  $_POST['email'];
+    $contactno = $_POST['contactno'];
+    $msg =  $_POST['msg'];
+    if(!empty($name) && !empty($email) && !empty($contactno) && !empty($msg) )
+        {  
           if(filter_var($email, FILTER_VALIDATE_EMAIL)==true)
            {          
-        		 $query = "INSERT INTO contact(name,email,contactno,msg) VALUES ('{$name}','{$email}','{$contactno}','{$msg}')"; 
-        		 $result = mysqli_query($conn,$query);
-          	     if(!$result)
-         			    {
-            			  die('Error, Please Try Again');  
-           			  }
+             $query = "INSERT INTO contact(name,email,contactno,msg) VALUES ('{$name}','{$email}','{$contactno}','{$msg}')"; 
+             $result = mysqli_query($conn,$query);
+                 if(!$result)
+                  {
+                    die('Error, Please Try Again');  
+                  }
           }
           else
           { 
@@ -24,13 +24,13 @@ error_reporting(1);
           } 
           /*  Email Validation is done and the values are inserted into the database. 
           */  
-    	}
+      }
       /*  if the user doesn't fill in all the details then, we return a message. 
       */
     else
-     	{
-      		echo " Please Fill In All Details";
-    	}	 
+      {
+          echo " Please Fill In All Details";
+      }  
 }
 ?>
 <!DOCTYPE html>
@@ -106,8 +106,8 @@ error_reporting(1);
             <div class="block-18 color-1 align-items-stretch">
               <div class="text">
                 <span>Served Over</span>
-                <strong class="number" data-number="1432805">0</strong>
-                <span>Children in 190 countries in the world</span>
+                <strong class="number" data-number="211201">0</strong>
+                <span>Children in India</span>
               </div>
             </div>
           </div>
@@ -115,7 +115,7 @@ error_reporting(1);
             <div class="block-18 color-2 align-items-stretch">
               <div class="text">
                 <h3 class="mb-4">Donate Money</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts.</p>
+                <p>An act of kindness lasts long. It gives you a feeling of satisfaction and fulfilment, apart from giving underprivileged children a chance to improve their circumstances and lessen the hardships that they face in their daily lives. </p>
                 <p><a href="donate/donn.php" class="btn btn-white px-3 py-2 mt-2">Donate Now</a></p>
               </div>
             </div>
@@ -124,7 +124,7 @@ error_reporting(1);
             <div class="block-18 color-3 align-items-stretch">
               <div class="text">
                 <h3 class="mb-4">Be a Volunteer</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts.</p>
+                <p>You can play an integral role in growing this movement and redefining the education system in our country.Share your skills to bring arts into a child’s learning also tutor or mentor a child once a week and do your bit for the society.</p>
                 <p><a href="donate/vol.php" class="btn btn-white px-3 py-2 mt-2">Be A Volunteer</a></p>
               </div>
             </div>
@@ -133,58 +133,18 @@ error_reporting(1);
       </div>
     </section>
 
-    <section class="ftco-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 d-flex services p-3 py-4 d-block">
-              <div class="icon d-flex mb-3"><span class="flaticon-donation-1"></span></div>
-              <div class="media-body pl-4">
-                <h3 class="heading">Make Donation</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-              </div>
-            </div>      
-          </div>
-          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 d-flex services p-3 py-4 d-block">
-              <div class="icon d-flex mb-3"><span class="flaticon-charity"></span></div>
-              <div class="media-body pl-4">
-                <h3 class="heading">Become A Volunteer</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-              </div>
-            </div>      
-          </div>
-          
-        </div>
-      </div>
-    </section>
-
-
+  
     <section class="ftco-section bg-light">
       <div class="container-fluid">
         <div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-5 heading-section ftco-animate text-center">
             <h2 class="mb-4">Our Causes</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p>Illiteracy in India is a problem which has complex dimensions attached to it. Illiteracy in India is more or less concerned with different forms of disparities that exist in the country. There are gender imbalances, income imbalances, state imbalances, caste imbalances, technological barriers which shape the literacy rates that exist in the country. India possesses the largest illiterate population.</p>
           </div>
         </div>
         <div class="row">
           <div class="col-md-12 ftco-animate">
             <div class="carousel-cause owl-carousel">
-              <div class="item">
-                <div class="cause-entry">
-                  <a href="#" class="img" style="background-image: url(images/cause-1.jpg);"></a>
-                  <div class="text p-3 p-md-4">
-                    <h3><a href="#">Clean water for the urban area</a></h3>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-                    <span class="donation-time mb-3 d-block">Last donation 1w ago</span>
-                    <div class="progress custom-progress-success">
-                      <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
-                  </div>
-                </div>
-              </div>
               <div class="item">
                 <div class="cause-entry">
                   <a href="#" class="img" style="background-image: url(images/cause-2.jpg);"></a>
@@ -195,7 +155,7 @@ error_reporting(1);
                     <div class="progress custom-progress-success">
                       <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+                    <span class="fund-raised d-block">₹1,00,000 raised of ₹2,50,000</span>
                   </div>
                 </div>
               </div>
@@ -203,13 +163,13 @@ error_reporting(1);
                 <div class="cause-entry">
                   <a href="#" class="img" style="background-image: url(images/cause-3.jpg);"></a>
                   <div class="text p-3 p-md-4">
-                    <h3><a href="#">Clean water for the urban area</a></h3>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+                    <h3><a href="#">Education for Underprivileged</a></h3>
+                    <p>India’s growth relies on a well-educated and skilled workforce. Improving education is a critical area of investment. A shabby foundation in primary education can overturn the lives of millions of its citizens.</p>
                     <span class="donation-time mb-3 d-block">Last donation 1w ago</span>
                     <div class="progress custom-progress-success">
                       <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+                    <span class="fund-raised d-block">₹2,20,000 raised of ₹5,00,000</span>
                   </div>
                 </div>
               </div>
@@ -217,99 +177,17 @@ error_reporting(1);
                 <div class="cause-entry">
                   <a href="#" class="img" style="background-image: url(images/cause-4.jpg);"></a>
                   <div class="text p-3 p-md-4">
-                    <h3><a href="#">Clean water for the urban area</a></h3>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
+                    <h3><a href="#">Old Age Support</a></h3>
+                    <p>Donate and help senior citizens for their nutrition, shelter, medical and ensuring their physical, mental, spiritual and emotional well-being of each senior citizen’s.</p>
                     <span class="donation-time mb-3 d-block">Last donation 1w ago</span>
                     <div class="progress custom-progress-success">
                       <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
+                    <span class="fund-raised d-block">₹3,00,000 raised of ₹5,00,000</span>
                   </div>
                 </div>
               </div>
-              <div class="item">
-                <div class="cause-entry">
-                  <a href="#" class="img" style="background-image: url(images/cause-5.jpg);"></a>
-                  <div class="text p-3 p-md-4">
-                    <h3><a href="#">Clean water for the urban area</a></h3>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-                    <span class="donation-time mb-3 d-block">Last donation 1w ago</span>
-                    <div class="progress custom-progress-success">
-                      <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="cause-entry">
-                  <a href="#" class="img" style="background-image: url(images/cause-6.jpg);"></a>
-                  <div class="text p-3 p-md-4">
-                    <h3><a href="#">Clean water for the urban area</a></h3>
-                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life</p>
-                    <span class="donation-time mb-3 d-block">Last donation 1w ago</span>
-                    <div class="progress custom-progress-success">
-                      <div class="progress-bar bg-primary" role="progressbar" style="width: 28%" aria-valuenow="28" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="fund-raised d-block">$12,000 raised of $30,000</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="ftco-section">
-      <div class="container">
-        <div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Latest Donations</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-            <div class="staff">
-              <div class="d-flex mb-4">
-                <div class="img" style="background-image: url(images/person_1.jpg);"></div>
-                <div class="info ml-4">
-                  <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                  <span class="position">Donated Just now</span>
-                  <div class="text">
-                    <p>Donated <span>$300</span> for <a href="#">Children Needs Food</a></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-            <div class="staff">
-              <div class="d-flex mb-4">
-                <div class="img" style="background-image: url(images/person_2.jpg);"></div>
-                <div class="info ml-4">
-                  <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                  <span class="position">Donated Just now</span>
-                  <div class="text">
-                    <p>Donated <span>$150</span> for <a href="#">Children Needs Food</a></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 d-flex mb-sm-4 ftco-animate">
-            <div class="staff">
-              <div class="d-flex mb-4">
-                <div class="img" style="background-image: url(images/person_3.jpg);"></div>
-                <div class="info ml-4">
-                  <h3><a href="teacher-single.html">Ivan Jacobson</a></h3>
-                  <span class="position">Donated Just now</span>
-                  <div class="text">
-                    <p>Donated <span>$250</span> for <a href="#">Children Needs Food</a></p>
-                  </div>
-                </div>
-              </div>
+              
             </div>
           </div>
         </div>
@@ -360,35 +238,6 @@ error_reporting(1);
             <span class="icon-search"></span>
           </div>
         </a>
-      </div>
-    </section>
-
-    
-    <section class="ftco-section-3 img" style="background-image: url(images/bg_3.jpg);">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row d-md-flex">
-        <div class="col-md-6 d-flex ftco-animate">
-          <div class="img img-2 align-self-stretch" style="background-image: url(images/bg_4.jpg);"></div>
-        </div>
-        <div class="col-md-6 volunteer pl-md-5 ftco-animate">
-          <h3 class="mb-3">Be a volunteer</h3>
-          <form action="#" class="volunter-form">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Name">
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Email">
-            </div>
-            <div class="form-group">
-              <textarea name="" id="" cols="30" rows="3" class="form-control" placeholder="Message"></textarea>
-            </div>
-            <div class="form-group">
-              <input type="submit" value="Send Message" class="btn btn-white py-3 px-5">
-            </div>
-          </form>
-        </div>          
-        </div>
       </div>
     </section>
 
